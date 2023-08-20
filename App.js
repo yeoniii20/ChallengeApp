2c12222e68bf98d382144058e9df43f61997f560
KwonSuyeon <commetoi_yeoni@naver.com>
2023-08-21 00:36:56 +0900

    feat: navigator 추가

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Login from "./constant/Login";
+import SiginUp from "./constant/SiginUp";
+
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Login />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="SiginUp" component={SiginUp} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
