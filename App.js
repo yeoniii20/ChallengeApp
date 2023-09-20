9008d0d1d8235514e4ee7f2767396e5b2318a69b
KwonSuyeon <commetoi_yeoni@naver.com>
2023-09-21 00:15:41 +0900

    fix: app 수정

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Login from "./constant/Login";
-import SiginUp from "./constant/SiginUp";
-
-import { createStackNavigator } from "@react-navigation/stack";
+import * as React from "react";
+import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import FriendsList from "./constant/friendsList";
+import Note from "./constant/note";
+import Statistics from "./constant/statistics";
+import Top from "./components/Top";
+import Bottom from "./components/Bottom";
+import Login from "./constant/Login";
+import Kakao from "./constant/kakao";
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
+// Stack object를 반환함. { Screen, Navigator }로 구성됨.
 
-export default function App() {
+function App() {
   return (
+    // NavigationContainer로 감싸야 함.
     <NavigationContainer>
-      <Stack.Navigator>
+      {/* <Top /> */}
+      <Stack.Navigator
+        initialRouteName="Note"
+        screenOptions={{
+          header: (props) => <Top {...props} />,
+        }}
+      >
+        <Stack.Screen name="FriendsList" component={FriendsList} />
+        <Stack.Screen name="Note" component={Note} />
+        <Stack.Screen name="Statistics" component={Statistics} />
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="SiginUp" component={SiginUp} />
+        <Stack.Screen name="Kakao" component={Kakao} />
       </Stack.Navigator>
+      <Bottom />
     </NavigationContainer>
   );
 }
+
+export default App;
