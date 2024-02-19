8299358cefbd1ea53992d8d9ad8399546f67b54c
KwonSuyeon <commetoi_yeoni@naver.com>
2024-02-20 01:07:13 +0900

    feat: toggleSwitch 생성

---
+++ components/ToggleSwitch/customToggleSwitch.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const CustomToggleSwitch = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const toggleSwitch = () => setIsEnabled(!isEnabled);
+
+  return (
+    <TouchableOpacity
+      style={[
+        styles.switch,
+        isEnabled ? styles.switchEnabled : styles.switchDisabled,
+      ]}
+      onPress={toggleSwitch}
+    >
+      <View
+        style={[
+          styles.thumb,
+          isEnabled ? styles.thumbEnabled : styles.thumbDisabled,
+        ]}
+      />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  switch: {
+    width: 50, // Track width
+    height: 30, // Track height
+    borderRadius: 15,
+    justifyContent: "center",
+    padding: 3,
+  },
+  switchEnabled: {
+    backgroundColor: "#F4595D",
+  },
+  switchDisabled: {
+    backgroundColor: "#969696",
+  },
+  thumb: {
+    width: 24, // Thumb width
+    height: 24, // Thumb height
+    borderRadius: 12,
+  },
+  thumbEnabled: {
+    alignSelf: "flex-end",
+    backgroundColor: "#fff",
+  },
+  thumbDisabled: {
+    alignSelf: "flex-start",
+    backgroundColor: "#fff",
+  },
+});
+
+export default CustomToggleSwitch;
---
+++ components/ToggleSwitch/toggleSwitch.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { View, Switch, StyleSheet } from "react-native";
+
+const ToggleSwitch = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  return (
+    <>
+      <Switch
+        trackColor={{ false: "#969696", true: "#F4595D" }}
+        thumbColor={isEnabled ? "#fff" : "#fff"}
+        ios_backgroundColor="#969696"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default ToggleSwitch;
