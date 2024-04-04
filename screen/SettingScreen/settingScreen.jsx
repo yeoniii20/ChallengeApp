c11dd8f4e38631368247a99f909cb4bf42db86bb
yeoniii20 <commetoi_yeoni@naver.com>
2024-04-04 23:46:03 +0900

    feat: add settingScreen

---
+++ screen/SettingScreen/settingScreen.jsx
@@ -0,0 +1,147 @@
+import React, { useEffect, useState } from "react";
+import styles from "./styles";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  Button,
+  Animated,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import SlimToggleSwitch from "../../components/ToggleSwitch/slimToggleSwitch";
+import RadioBtn2 from "../../components/Btn/RadioBtn2";
+
+const SETTING_ALARM = [
+  {
+    id: 1,
+    title: "시작 20분 전 알림",
+    description: "준비 과정 시작 20분 전, 알림을 보내드려요",
+  },
+  {
+    id: 2,
+    title: "시작 10분 전 알림",
+    description: "준비 과정 시작 10분 전, 알림을 보내드려요",
+  },
+  {
+    id: 3,
+    title: "외출 10분 전 알림",
+    description: "외출 10분 전, 알림을 보내드려요",
+  },
+  {
+    id: 4,
+    title: "외출 5분 전 알림",
+    description: "외출 5분 전, 알림을 보내드려요",
+  },
+];
+
+const SettingScreen = () => {
+  const windowHeight = Dimensions.get("window").height;
+  const navigation = useNavigation();
+  const [selectedValue, setSelectedValue] = useState("단호하게");
+
+  const handleRadioChange = (value) => {
+    setSelectedValue(value);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.iconBox}
+        >
+          <Image
+            source={require("../../assets/img/Icon/arrow.png")}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        <Text style={styles.text}>앱 설정</Text>
+        <View style={styles.iconBox}></View>
+      </View>
+      <ScrollView style={{ backgroundColor: "#fff" }}>
+        <View style={styles.content}>
+          <Text style={styles.contentTitle}>알림</Text>
+          {SETTING_ALARM.map((item) => (
+            <View style={styles.padding}>
+              <View style={styles.contentContainer}>
+                <View style={{ gap: 6 }}>
+                  <Text style={styles.contentText}>{item.title}</Text>
+                  <Text style={styles.contentDes}>{item.description}</Text>
+                </View>
+                <View>
+                  <SlimToggleSwitch
+                    id={item.id}
+                    isEnable={false}
+                    onClick={() => {}}
+                  />
+                </View>
+              </View>
+              <View style={[styles.line, { height: 2 }]} />
+            </View>
+          ))}
+          <View style={styles.padding}>
+            <View style={styles.contentContainer}>
+              <View style={{ gap: 6 }}>
+                <Text style={styles.contentText}>준비 과정 관리 알람</Text>
+                <Text style={styles.contentDes}>
+                  준비 과정 활성화를 위한 알림을 보내드려요
+                </Text>
+                <View style={styles.timeSection}>
+                  <Text style={styles.time}>00 : 00 AM</Text>
+                </View>
+              </View>
+              <View>
+                <SlimToggleSwitch id={0} isEnable={true} onClick={() => {}} />
+              </View>
+            </View>
+          </View>
+          <View style={[styles.line, { height: 4 }]} />
+          <Text style={styles.contentTitle}>알림 어조</Text>
+          <View style={styles.padding}>
+            <Text style={[styles.contentText, { marginBottom: 8 }]}>어조</Text>
+            <View style={{ flexDirection: "row", gap: 24 }}>
+              <RadioBtn2
+                label="단호하게"
+                value="단호하게"
+                checked={selectedValue === "단호하게"}
+                onCheck={handleRadioChange}
+              />
+              <RadioBtn2
+                label="부드럽게"
+                value="부드럽게"
+                checked={selectedValue === "부드럽게"}
+                onCheck={handleRadioChange}
+              />
+            </View>
+          </View>
+          <View style={[styles.line, { height: 4 }]} />
+          <Text style={styles.contentTitle}>음량</Text>
+          <View style={styles.padding}>
+            <View style={styles.contentContainer}>
+              <View style={{ gap: 6 }}>
+                <Text style={styles.contentText}>음량 설정</Text>
+                <Text style={styles.contentDes}>
+                  시스템 자체 음량 설정으로 이동해요
+                </Text>
+              </View>
+              <View>
+                <TouchableOpacity>
+                  <Image
+                    source={require("../../assets/img/Icon/arrowRight.png")}
+                    style={styles.icon}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={[styles.line, { height: 2 }]} />
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default SettingScreen;
---
+++ screen/SettingScreen/styles.js
@@ -0,0 +1,82 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+  },
+  section: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#F5F5F5",
+    paddingTop: 59,
+    paddingBottom: 12,
+    paddingLeft: 24,
+    paddingRight: 24,
+    borderBottomColor: "#4B4B4B",
+    borderBottomWidth: 2,
+  },
+  text: {
+    color: "#1B1B1B",
+    fontSize: 16,
+    fontFamily: "Pretendard-SemiBold",
+    alignSelf: "center",
+    right: 12,
+  },
+  icon: {
+    height: 24,
+    width: 24,
+  },
+  iconBox: { alignSelf: "center" },
+  padding: { paddingLeft: 24, paddingRight: 24 },
+  contentContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  content: {
+    marginTop: 24,
+    gap: 8,
+    marginBottom: 55,
+  },
+  contentTitle: {
+    color: "#1B1B1B",
+    fontFamily: "Pretendard-Bold",
+    fontSize: 20,
+    marginBottom: 20,
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  contentText: {
+    color: "#1B1B1B",
+    fontFamily: "Pretendard-SemiBold",
+    fontSize: 16,
+  },
+  contentDes: {
+    color: "#636363",
+    fontFamily: "Pretendard-Regular",
+    fontSize: 12,
+    marginBottom: 4,
+  },
+  line: {
+    width: "100%",
+    backgroundColor: "#4B4B4B",
+    marginBottom: 10,
+    marginTop: 12,
+  },
+  time: {
+    fontFamily: "Pretendard-SemiBold",
+    fontSize: 16,
+    color: "#969696",
+    textAlign: "center",
+    paddingTop: 4,
+    paddingBottom: 4,
+  },
+  timeSection: {
+    borderColor: "#B9B9B9",
+    borderWidth: 2,
+    width: 100,
+    borderRadius: 4,
+    backgroundColor: "#E1E1E1",
+  },
+});
