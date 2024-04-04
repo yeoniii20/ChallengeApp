303f523f1c42172b1c601ad6ae8d1f49e120e96d
yeoniii20 <commetoi_yeoni@naver.com>
2024-04-04 23:45:52 +0900

    feat: add ProcessScreen

---
+++ screen/ProcessScreen/processScreen.jsx
@@ -0,0 +1,209 @@
+import React, { useState } from "react";
+import styles from "./styles";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Image,
+} from "react-native";
+import LargeBtn from "../../components/Button/largeBtn";
+import TodoCard from "../../components/Card/todoCard";
+import PlusBtn from "../../components/Button/plusBtn";
+import { useNavigation } from "@react-navigation/native";
+import BottomSheetScreen from "../../components/BottomSheet/BottomSheetScreen";
+
+const TODO_LIST = [
+  {
+    title: "세수하기",
+    time: "3",
+    imagePath: "../../assets/img/Icon/clothing.png",
+  },
+  {
+    title: "옷입기",
+    time: "2",
+    imagePath: "../../assets/img/Icon/clothing.png",
+  },
+];
+
+const COMMUNITY_SORT = [
+  { title: "최신순", data: "LATEST" },
+  { title: "조회순", data: "VIEW" },
+];
+
+const ProcessScreen = () => {
+  const navigation = useNavigation();
+
+  const [isInputFocused, setInputFocused] = useState < boolean > false;
+  const [inputValue, setInputValue] = useState < string > "";
+  const [open, setOpen] = useState < boolean > false;
+  const [board, setBoardSort] = useState < string > "";
+
+  const handleSortClick = () => {
+    setOpen(!open);
+    console.log("정렬하시게씀까");
+  };
+
+  const handleCategorySelect = (data) => {
+    setBoardSort(data);
+  };
+
+  const handleFocus = () => {
+    setInputFocused(true);
+  };
+
+  const handleBlur = () => {
+    setInputFocused(false);
+  };
+
+  const handleCloseBtnClick = () => {
+    navigation.navigate("Home");
+  };
+
+  const handleInputChange = (text) => {
+    // 9자 이하일 때만 상태를 업데이트
+    if (text.length <= 9) {
+      setInputValue(text);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <TouchableOpacity onPress={handleCloseBtnClick} style={{}}>
+          <Image
+            source={require("../../assets/img/Icon/close.png")}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        <Text style={styles.text}>준비 과정 추가</Text>
+      </View>
+      {/* 준비 과정 이름 입력 */}
+      <View style={styles.middleSection}>
+        <Text style={styles.inputTitle}>준비 과정 이름</Text>
+        <TextInput
+          placeholder={"과정 이름"}
+          placeholderTextColor={"#B9B9B9"}
+          value={inputValue}
+          onChangeText={handleInputChange}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          maxLength={9}
+          style={styles.textInput}
+        />
+        <Text style={styles.textCount}>{inputValue.length} / 9</Text>
+        {/* 이동 시간 및 도착 시간 */}
+        <View style={styles.timeContainer}>
+          <View style={styles.timeSection}>
+            <View style={{ flexDirection: "row", gap: 8 }}>
+              <Text style={styles.topText}>이동 시간:</Text>
+              <Text style={[styles.topSubText, { color: "#B9B9B9" }]}>
+                00 시간
+              </Text>
+              <Text style={[styles.topSubText, , { color: "#B9B9B9" }]}>
+                00 분
+              </Text>
+            </View>
+            <TouchableOpacity onPress={() => {}} style={{}}>
+              <Image
+                source={require("../../assets/img/Icon/control.png")}
+                style={styles.icon}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.timeSection}>
+            <View style={{ flexDirection: "row", gap: 8 }}>
+              <Text style={styles.topText}>도착 시간:</Text>
+              <Text style={[styles.topSubText, { color: "#B9B9B9" }]}>
+                00 시간
+              </Text>
+              <Text style={[styles.topSubText, , { color: "#B9B9B9" }]}>
+                00 분
+              </Text>
+            </View>
+            <TouchableOpacity onPress={() => {}} style={{}}>
+              <Image
+                source={require("../../assets/img/Icon/control.png")}
+                style={styles.icon}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      <View style={styles.line} />
+      {/* todo card 추가 */}
+      <ScrollView style={styles.middleSection}>
+        <View style={{ gap: 8 }}>
+          {TODO_LIST.map((itme) => (
+            <>
+              <TodoCard
+                title={itme.title}
+                time={itme.time}
+                imagePath={itme.imagePath}
+              />
+            </>
+          ))}
+
+          <View style={{ height: 8 }} />
+        </View>
+        <View style={styles.plusButtonContainer}>
+          <PlusBtn
+            color={"#9BED94"}
+            width={40}
+            height={40}
+            onPress={() => {}}
+          />
+        </View>
+      </ScrollView>
+      <View
+        style={{ position: "relative", bottom: 0, backgroundColor: "#D5F8D1" }}
+      >
+        <View style={styles.line} />
+        <View style={[styles.middleSection, { paddingBottom: 11 }]}>
+          <View style={styles.bottomSection}>
+            <View style={styles.bottomBox}>
+              <Text style={styles.bottomText}>소요 시간:</Text>
+              <Text style={[styles.bottomSubText, { color: "#B9B9B9" }]}>
+                총
+              </Text>
+              <Text style={[styles.bottomSubText, { color: "#B9B9B9" }]}>
+                00 시간
+              </Text>
+              <Text style={[styles.bottomSubText, { color: "#B9B9B9" }]}>
+                00 분
+              </Text>
+            </View>
+            <View style={styles.bottomBox}>
+              <Text style={styles.bottomText}>시작 시간:</Text>
+              <Text style={[styles.bottomSubText, { color: "#B9B9B9" }]}>
+                -- : 00 AM
+              </Text>
+            </View>
+          </View>
+          <LargeBtn
+            text={"저장하기"}
+            onClick={() => {}}
+            backgroundColor={""}
+            isDisable={true}
+          />
+          <Text style={styles.noticeText}>
+            과정 이름, 이동 시간, 도착 시간, 할 일 카드(1개 이상)를 모두 입력해
+            주세요.
+          </Text>
+        </View>
+      </View>
+      {open && (
+        <BottomSheetScreen
+          title={"정렬기준"}
+          data={COMMUNITY_SORT}
+          modalVisible={open}
+          modalDisable={handleSortClick}
+          onCategorySelect={handleCategorySelect}
+        />
+      )}
+    </View>
+  );
+};
+
+export default ProcessScreen;
---
+++ screen/ProcessScreen/styels.js
@@ -0,0 +1,116 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#D5F8D1",
+  },
+  section: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    backgroundColor: "#F5F5F5",
+    paddingTop: 57,
+    paddingBottom: 10,
+    paddingLeft: 24,
+    paddingRight: 24,
+    borderBottomColor: "#4B4B4B",
+    borderBottomWidth: 2,
+  },
+  inputTitle: {
+    padding: 8,
+    color: "#636363",
+    fontSize: 10,
+    fontWeight: "400",
+  },
+  text: {
+    flex: 1,
+    textAlign: "center",
+    alignSelf: "center",
+    right: 10,
+    color: "#1B1B1B",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  textInput: {
+    backgroundColor: "#fff",
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingTop: 8,
+    paddingBottom: 8,
+    borderBottomColor: "#B9B9B9",
+    borderBottomWidth: 2,
+    height: 50,
+    fontSize: 28,
+  },
+  textCount: {
+    textAlign: "right",
+    color: "#636363",
+    fontSize: 12,
+    fontWeight: "400",
+  },
+  topText: {
+    color: "#1B1B1B",
+    fontSize: 16,
+    fontWeight: "400",
+  },
+  topSubText: {
+    fontSize: 16,
+    fontWeight: "400",
+  },
+  icon: {
+    height: 24,
+    width: 24,
+    alignSelf: "center",
+  },
+  plusButtonContainer: {
+    marginTop: 16,
+    marginBottom: 32,
+    alignItems: "center",
+  },
+  line: {
+    height: 2,
+    width: "100%",
+    backgroundColor: "#4B4B4B",
+  },
+  timeContainer: {
+    paddingRight: 8,
+    paddingLeft: 8,
+    flexDirection: "column",
+    gap: 8,
+    marginTop: 8,
+  },
+  timeSection: { flexDirection: "row", justifyContent: "space-between" },
+  bottomText: {
+    color: "#1B1B1B",
+    fontSize: 20,
+    fontWeight: "400",
+  },
+  bottomSubText: {
+    fontSize: 20,
+    fontWeight: "400",
+  },
+  middleSection: {
+    paddingRight: 24,
+    paddingLeft: 24,
+    paddingTop: 16,
+    paddingBottom: 16,
+  },
+  noticeText: {
+    padding: 8,
+    color: "#636363",
+    fontSize: 10,
+    fontFamily: "Pretendard-Medium",
+  },
+  bottomSection: {
+    paddingRight: 8,
+    paddingLeft: 8,
+    gap: 8,
+    marginBottom: 24,
+  },
+  bottomBox: {
+    justifyContent: "flex-end",
+    gap: 8,
+    flexDirection: "row",
+  },
+});
