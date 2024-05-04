a75614a42b506cffc7c9b493a9f8d283c81e45e8
yeoniii20 <commetoi_yeoni@naver.com>
2024-05-05 03:30:24 +0900

    fix: header 수정

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { TouchableOpacity } from "react-native";
 import { Icon } from "react-native-elements";
 import Home from "./screen/Home/home";
 import Settings from "./screen/Setting/setting";
@@ -8,11 +10,36 @@ import Messages from "./screen/Messages/messages";
 
 const Tab = createBottomTabNavigator();
 
+function CustomBackButton() {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      onPress={() => navigation.goBack()}
+      style={{ marginLeft: 10 }}
+    >
+      <Icon
+        name="chevron-left"
+        type="material-community"
+        size={30}
+        color="#fff"
+      />
+    </TouchableOpacity>
+  );
+}
+
 function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
+          headerStyle: {
+            backgroundColor: "#f4511e", // 헤더의 배경색
+          },
+          headerTintColor: "#fff", // 헤더의 글자색
+          headerTitleStyle: {
+            fontWeight: "bold", // 헤더 타이틀의 글자 스타일
+          },
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
@@ -23,11 +50,11 @@ function App() {
             } else if (route.name === "Setting") {
               iconName = "settings";
             }
-
             return (
               <Icon name={iconName} type="material" color={color} size={size} />
             );
           },
+          // headerShown: false,
         })}
         tabBarOptions={{
           activeTintColor: "tomato",
@@ -35,8 +62,20 @@ function App() {
         }}
       >
         <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Messages" component={Messages} />
-        <Tab.Screen name="Setting" component={Settings} />
+        <Tab.Screen
+          name="Messages"
+          component={Messages}
+          options={{
+            headerLeft: () => <CustomBackButton />,
+          }}
+        />
+        <Tab.Screen
+          name="Setting"
+          component={Settings}
+          options={{
+            headerLeft: () => <CustomBackButton />,
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
